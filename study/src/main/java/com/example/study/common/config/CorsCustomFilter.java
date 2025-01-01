584dfd078e7ebed2f4d319ed0b5e9b486dc365da
승엽 <tmdduqflfl@naver.com>
2025-01-01 20:07:21 +0900

    Feat: cors 해결 및 커스텀 익셉션

---
+++ study/src/main/java/com/example/study/common/config/CorsCustomFilter.java
@@ -0,0 +1,24 @@
+package com.example.study.common.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class CorsCustomFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request,
+                                    final HttpServletResponse response,
+                                    final FilterChain filterChain) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ study/src/main/java/com/example/study/global/exception/exceptions/CustomException.java
@@ -0,0 +1,14 @@
+package com.example.study.global.exception.exceptions;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final CustomErrorCode customErrorCode;
+
+    public CustomException(CustomErrorCode customErrorCode) {
+        super(customErrorCode.getCustomCode() + ": " + customErrorCode.getMessage());
+        this.customErrorCode = customErrorCode;
+    }
+}
